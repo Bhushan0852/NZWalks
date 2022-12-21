85d9c4aa6bd440e847f57eca96102a2e06c854f4
bhushan.doble <bhushan.doble@zconsolutions.com>
2022-12-22 01:38:31 +0530

    End of section 3

---
+++ NZWalks/NZWalks.API/Controllers/RegionController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Repository;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+
+        public RegionController(IRegionRepository regionRepository)
+        {
+            this.regionRepository = regionRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegion()
+        {
+            var regoins = await regionRepository.GetAllAsync();
+
+            //var dtoRegion = new List<Models.DTOs.Region>();
+            //regoins.ToList().ForEach(r =>
+            //{
+            //    var dtoReg = new Models.DTOs.Region()
+            //    {
+            //        Id = r.Id,
+            //        Name = r.Name,
+            //        Area = r.Area,
+            //        Code = r.Code,
+            //        Lat = r.Lat,
+            //        Long = r.Long,
+            //        Population = r.Population
+            //    };
+            //    dtoRegion.Add(dtoReg);  
+            //});
+
+
+
+            return Ok(regoins);
+        }
+    }
+}
--- NZWalks/NZWalks.API/Controllers/WeatherForecastController.cs
+++ NZWalks/NZWalks.API/Controllers/WeatherForecastController.cs
@@ -4,7 +4,7 @@ namespace NZWalks.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : Controller
+    public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
